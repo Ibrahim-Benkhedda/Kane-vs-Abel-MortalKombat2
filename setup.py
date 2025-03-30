2aa26212d6e9fa95857704a56a6512cc75c90221
Ibrahim Ben <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2025-03-30 17:30:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     author="Ibrahim Benkhedda",
-    description="AI agents for Mortal Kombat II: Genesis",
+    description="AI agents evaluation platform in Mortal Kombat II: Genesis",
+    install_requires=[
+        "stable_baselines3==2.4.0",
+        "stable-retro==0.9.2",
+        "gymnasium==1.0.0",
+        "optuna==4.1.0", 
+        "torch==2.5.1",
+        "tensorboard==2.18.0",
+    ],
 )
 
